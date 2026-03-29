b66c354db4ed04abb8174a8b337f770010b35439
dencoseca <leonbrown@Leons-MacBook.local>
2026-03-29 12:48:32 +0100

    Upgrade dependencies: `@vitejs/plugin-react` to `6.0.1`, `vite` to `8.0.3`, and related packages in `pnpm-lock.yaml`.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { fileURLToPath, URL } from "node:url";
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
